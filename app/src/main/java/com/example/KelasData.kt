f02c5611ca3d8fb5c285d9c68d2f75265f6795bb
kyy08 <kyyelzz1@gmail.com>
2024-10-13 09:14:42 +0700

    Dengan menggunakan data class, kita bisa mengelola data dengan cara yang lebih efisien dan mendapatkan beberapa metode standar secara otomatis yang memudahkan dalam memanipulasi objek.

--- app/src/main/java/com/example/KelasData.kt
+++ app/src/main/java/com/example/KelasData.kt
@@ -5,17 +5,3 @@ data class DataClass(
     var email: String
 )
 
-fun main() {
-    val data = DataClass(1,"Wahyu@gmail.com")
-    println(data)
-
-    val data2 = DataClass(1, "Makan@gmail.com")
-    println(data == data2)
-
-    val data3 = data.copy()
-    println(data3)
-
-    val data4 = data.copy(email = "aurora@gmail.com")
-    println(data4)
-
-}
